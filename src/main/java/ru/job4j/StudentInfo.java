ff2a95481efbdb28aa5333123fb64a935bd08789
Nariman00 <gasanovnar0210@gmail.com>
2022-01-28 17:57:00 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Gasanov Nariman");
+        System.out.println("02.10.2000");
+    }
+
+}
